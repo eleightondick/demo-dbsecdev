742f1f2a145f41bf02406f1afbd2e17699c6e248
David Thole <david@thedarktrumpet.com>
2019-10-26 12:07:40 -0500

    Adding two controllers, one for insecure and one for secure operations

--- SecurityApplication/SecurityApplication/Controllers/InsecureController.cs
+++ SecurityApplication/SecurityApplication/Controllers/InsecureController.cs
@@ -6,8 +6,9 @@ using System.Web.Mvc;
 
 namespace SecurityApplication.Controllers
 {
-    public class HomeController : Controller
+    public class InsecureController : Controller
     {
+        // GET: Insecure
         public ActionResult Index()
         {
             return View();
--- SecurityApplication/SecurityApplication/Controllers/SecureController.cs
+++ SecurityApplication/SecurityApplication/Controllers/SecureController.cs
@@ -6,8 +6,9 @@ using System.Web.Mvc;
 
 namespace SecurityApplication.Controllers
 {
-    public class HomeController : Controller
+    public class SecureController : Controller
     {
+        // GET: Secure
         public ActionResult Index()
         {
             return View();
