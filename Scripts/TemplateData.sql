87594d2c2b71fc5cdfb28730b68066458018c272
David Thole <david@thedarktrumpet.com>
2019-10-26 17:32:33 -0500

    Fixing a bug in the person generation/view and enabling the call to the iterable.  Adding a bootstrap script as well

---
+++ Scripts/TemplateData.sql
@@ -0,0 +1,65 @@
+-- Cleanup Records
+DELETE FROM dbo.People
+DELETE FROM dbo.Companies
+
+-- Insert Companies
+INSERT INTO dbo.Companies
+(
+    Name
+)
+VALUES
+(N'Water Nation' -- Name - nvarchar(max)
+    )
+
+INSERT INTO dbo.Companies
+(
+    Name
+)
+VALUES
+(N'Air Nomads' -- Name - nvarchar(max)
+    )
+
+INSERT INTO dbo.Companies
+(
+    Name
+)
+VALUES
+(N'Order of the White Lotus' -- Name - nvarchar(max)
+    )
+
+-- Insert People
+INSERT INTO dbo.People
+(
+    FirstName,
+    LastName,
+    Company_Id
+)
+VALUES
+(   N'General', -- FirstName - nvarchar(max)
+    N'Iroh', -- LastName - nvarchar(max)
+    (SELECT TOP 1 Id FROM dbo.Companies WHERE Name = 'Order of the White Lotus')    -- Company_Id - int
+)
+
+INSERT INTO dbo.People
+(
+    FirstName,
+    LastName,
+    Company_Id
+)
+VALUES
+(   N'Avatar', -- FirstName - nvarchar(max)
+    N'Aang', -- LastName - nvarchar(max)
+    (SELECT TOP 1 Id FROM dbo.Companies WHERE Name = 'Air Nomads')    -- Company_Id - int
+    )
+
+INSERT INTO dbo.People
+(
+    FirstName,
+    LastName,
+    Company_Id
+)
+VALUES
+(   N'Katara', -- FirstName - nvarchar(max)
+    N'', -- LastName - nvarchar(max)
+    (SELECT TOP 1 Id FROM dbo.Companies WHERE Name = 'Water Nation')    -- Company_Id - int
+)
