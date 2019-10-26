649f2bea3ac41114982a1a80ddd9ea79e1e4fdc9
David Thole <david@thedarktrumpet.com>
2019-10-26 12:20:36 -0500

    Adding EntityFramework and some simple bindings to it

--- SecurityApplication/SecurityApplication/Controllers/SecureController.cs
+++ SecurityApplication/SecurityApplication/Controllers/SecureController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SecurityApplication.Models;
 
 namespace SecurityApplication.Controllers
 {
     public class SecureController : Controller
     {
+
+        public SecureController()
+        {
+
+        }
         // GET: Secure
         public ActionResult Index()
         {
---
+++ SecurityApplication/SecurityApplication/Models/EFDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SecurityApplication.Models
+{
+    public class EFDbContext : DbContext
+    {
+        public EFDbContext(string connString) : base(connString)
+        {
+
+        }
+
+        public EFDbContext()
+        {
+
+        }
+
+        public virtual DbSet<Person> People { get; set; }
+        public virtual  DbSet<Company> Companies { get; set; }
+    }
+}
