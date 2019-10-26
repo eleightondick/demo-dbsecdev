f2d431425767d1b56418cc4a432bcb40bf73a921
David Thole <david@thedarktrumpet.com>
2019-10-26 12:15:28 -0500

    Adding a very simple model

---
+++ SecurityApplication/SecurityApplication/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SecurityApplication.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public virtual Company Company { get; set; }
+
+        [NotMapped] public string FullName => FirstName + " " + LastName;
+    }
+}
