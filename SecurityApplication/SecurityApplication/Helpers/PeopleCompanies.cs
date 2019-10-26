a3a72b76bc2f7b26fb052c731175b6313852d3fa
David Thole <david@thedarktrumpet.com>
2019-10-26 14:38:03 -0500

    Adding company name as a select list and architecuture

--- SecurityApplication/SecurityApplication/Helpers/PeopleCompanies.cs
+++ SecurityApplication/SecurityApplication/Helpers/PeopleCompanies.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 using SecurityApplication.Models;
 
 namespace SecurityApplication.Helpers
@@ -10,11 +11,18 @@ namespace SecurityApplication.Helpers
     {
         public IEnumerable<Person> People;
         public IEnumerable<Company> Companies;
+        public SelectList CompanyList;
 
         public PeopleCompanies(EFDbContext context)
         {
             this.People = context.People;
             this.Companies = context.Companies;
+            BuildSelectList();
+        }
+
+        public void BuildSelectList()
+        {
+            this.CompanyList = new SelectList(Companies, "Id", "Name");
         }
     }
 }
