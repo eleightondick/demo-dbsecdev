bbdb73b9b6593a9f804ff606d0db8eb5a0290aa9
David Thole <david@thedarktrumpet.com>
2019-10-27 12:30:26 -0500

    Adding a checkbox to the XSS page to allow for JS conversion

--- SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.ts
+++ SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.ts
@@ -1,20 +1,7 @@
 ﻿class Comments {
-    public static loadTable(allowInsecure: boolean) {
-        $.ajax({
-            type: "GET",
-            cache: false,
-            data: { allowInsecure },
-            url: "/Comments/GetTable",
-            success(data) {
-                $("#CommentsTable").html(data);
-            },
-            error(data) {
-                alert("An error happened loading the table: " + data);
-            }
-        });
-    }
+    public static saveComment(event: Event) {
+        event.preventDefault();
 
-    public static saveComment() {
-
+        $("#commentFormEntry").submit();
     }
 }
