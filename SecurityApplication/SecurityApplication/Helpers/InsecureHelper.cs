f6de344635e8c20bb3e035156caefb905951f22f
David Thole <david@thedarktrumpet.com>
2019-10-26 21:36:18 -0500

    Adding a helper for the insecure section of the app

---
+++ SecurityApplication/SecurityApplication/Helpers/InsecureHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SecurityApplication.Models;
+
+namespace SecurityApplication.Helpers
+{
+    public class InsecureHelper
+    {
+        public IEnumerable<Person> People { get; set; }
+        public string Statement { get; set; }
+
+        public InsecureHelper(EFDbContext context)
+        {
+            People = context.People;
+        }
+    }
+}
