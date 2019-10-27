fd5b07f1e368dcfbdce0c1d7781a8bb4628d1d1d
David Thole <david@thedarktrumpet.com>
2019-10-27 11:51:36 -0500

    Updating typescript with base content, adding the .js and .js.map to the ignore, but now need to remove them

--- SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.js
+++ SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.js
@@ -1,6 +1,22 @@
 var Comments = /** @class */ (function () {
     function Comments() {
     }
+    Comments.loadTable = function (allowInsecure) {
+        $.ajax({
+            type: "GET",
+            cache: false,
+            data: { allowInsecure: allowInsecure },
+            url: "/Comments/GetTable",
+            success: function (data) {
+                $("#CommentsTable").html(data);
+            },
+            error: function (data) {
+                alert("An error happened loading the table: " + data);
+            }
+        });
+    };
+    Comments.saveComment = function () {
+    };
     return Comments;
 }());
 //# sourceMappingURL=Comments.js.map
--- SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.ts
+++ SecurityApplication/SecurityApplication/Scripts/SecurityApplication/Comments.ts
@@ -1,3 +1,20 @@
 ﻿class Comments {
+    public static loadTable(allowInsecure: boolean) {
+        $.ajax({
+            type: "GET",
+            cache: false,
+            data: { allowInsecure },
+            url: "/Comments/GetTable",
+            success(data) {
+                $("#CommentsTable").html(data);
+            },
+            error(data) {
+                alert("An error happened loading the table: " + data);
+            }
+        });
+    }
 
+    public static saveComment() {
+
+    }
 }
