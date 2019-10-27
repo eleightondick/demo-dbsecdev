c5adf5c5fdfc6c7ccef1abee73dbfb1079ff5b91
David Thole <david@thedarktrumpet.com>
2019-10-27 11:56:40 -0500

    Adding bundle to load the javascript, and adding bundle to the layout, things are more or less linked up properly now

--- SecurityApplication/SecurityApplication/App_Start/BundleConfig.cs
+++ SecurityApplication/SecurityApplication/App_Start/BundleConfig.cs
@@ -25,6 +25,9 @@ namespace SecurityApplication
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/SecurityApplication").Include(
+                "~/Scripts/SecurityApplication/*.js"));
         }
     }
 }
